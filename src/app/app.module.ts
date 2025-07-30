8c594899d0ac0acdaeb9725e3bed4d23f7b3d4a0
Dwydee <158499218+Dwydee@users.noreply.github.com>
2025-07-30 15:12:15 +0400

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { register } from 'swiper/element/bundle';
 
+
+register();
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,6 +1,10 @@
 import { WorkingHours } from './../repeatable_components/working-hours/working-hours';
 import { Component } from '@angular/core';
 import { OurPricing } from "../repeatable_components/our-pricing/our-pricing";
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-home-page',
@@ -9,7 +13,8 @@ import { OurPricing } from "../repeatable_components/our-pricing/our-pricing";
     OurPricing
 ],
   templateUrl: './home-page.html',
-  styleUrls: ['./home-page.scss']
+  styleUrls: ['./home-page.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePage {
 
