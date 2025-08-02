5836b0248386d18d4ba64793d7b6861f2bce0189
Dwydee <158499218+Dwydee@users.noreply.github.com>
2025-08-02 19:13:02 +0400

    Fully updated navbar and added responsive to it, removed all the warnings and fixed routing

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss'
 })
 export class NavBar {
   isMenuOpen = false;
+  isMoreOpen = false;
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  toggleMore(): void {
+    this.isMoreOpen = !this.isMoreOpen;
   }
 }
