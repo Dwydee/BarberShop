26c7d9c54c25aa3c01ed94c4a0f079f0c55041c2
Dwydee <158499218+Dwydee@users.noreply.github.com>
2025-12-17 18:41:02 +0400

    Redesign Contact Us page layout and styles
    Refactored the Contact Us page structure and styling for improved layout and responsiveness. Updated HTML to use semantic sections, added a new hero section, reworked the image slider with Swiper and breakpoints, and introduced a new contact form. Updated SCSS for new layout, slider, and form styles. Adjusted app and global styles for better page structure. Minor responsive improvements in home page SCSS.

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,7 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { WorkingHours } from "../repeatable_components/working-hours/working-hours";
-import { register } from 'swiper/element/bundle';
-import { Swiper } from 'swiper/types';
+
 
 @Component({
   selector: 'app-contactus',
@@ -10,15 +9,45 @@ import { Swiper } from 'swiper/types';
   imports: [WorkingHours],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
+
 export class ContactusComponent {
-spaceBetween = 3;
-
-   onProgress(event: CustomEvent<[Swiper, number]>) {
-    const [swiper, progress] = event.detail;
-    console.log(progress);
-   }
-
-    onSlideChange() {
-    console.log('slide changed');
+  breakpointsConfig = {
+  320: {
+    loop: true,
+    autoplay: true,
+    speed: 800,
+    a11y: true,
+    allowTouchMove: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1
+  },
+  768: {
+    loop: true,
+    autoplay: true,
+    speed: 1000,
+    a11y: true,
+    allowTouchMove: true,
+    slidesPerView: 2,
+    slidesPerGroup: 2
+  },
+  1400: {
+    loop: true,
+    autoplay: true,
+    speed: 1200,
+    a11y: true,
+    allowTouchMove: true,
+    slidesPerView: 3,
+    slidesPerGroup: 3
+  },
+  1920: {
+    loop: true,
+    autoplay: true,
+    speed: 1400,
+    a11y: true,
+    allowTouchMove: false,
+    slidesPerView: 4,
+    slidesPerGroup: 3
   }
+
+  };
 }
